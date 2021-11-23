a1b06d54e7e5b99e154fae21e3a1847aff6aa974
Renato D Schneider <renatodaltiba@gmail.com>
2021-11-23 10:04:36 -0400

    Header Car Details

--- App.tsx
+++ App.tsx
@@ -12,9 +12,11 @@ import {
 
 import AppLoading from "expo-app-loading";
 
-import { Home } from "./src/screens/Home";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/styles/theme";
+
+import { Home } from "./src/screens/Home";
+import { CarDetails } from "./src/screens/CarDetails";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -31,7 +33,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      <CarDetails />
     </ThemeProvider>
   );
 }
