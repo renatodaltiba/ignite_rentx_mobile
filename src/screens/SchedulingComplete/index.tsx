68a3f8ff77356b2f79a699f4b89ed0f00d96585c
Renato D Schneider <renatodaltiba@gmail.com>
2021-11-23 18:10:55 -0400

    Interface de Agendamento Concluido

---
+++ src/screens/SchedulingComplete/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useWindowDimensions } from "react-native";
+
+import { Container, Content, Message, Title, Footer } from "./styles";
+import LogoSvg from "../../assets/icons/logo_background_gray.svg";
+import DoneSvg from "../../assets/icons/done.svg";
+import { ConfirmButton } from "../../components/ConfirmButton";
+
+export function SchedulingComplete() {
+  const { width } = useWindowDimensions();
+
+  return (
+    <Container>
+      <LogoSvg width={width} />
+
+      <Content>
+        <DoneSvg width={80} height={80} />
+        <Title>Carro alugado!</Title>
+
+        <Message>
+          Agora você só precisa ir {"\n"}
+          até a concessionária da RENTX {"\n"}
+          pegar o seu automóvel.
+        </Message>
+
+        <Footer>
+          <ConfirmButton title="OK" />
+        </Footer>
+      </Content>
+    </Container>
+  );
+}
